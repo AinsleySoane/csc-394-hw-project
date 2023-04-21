95510f1cffec5cb2b828f4a1867c0b184f74e8b0
Ainsley <ainsleysoane@gmail.com>
2023-04-21 17:26:59 -0500

    hw9

--- students/models.py
+++ students/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
+
+    class Course(models.Model):
+        department = models.CharField(max_length=60)
+        number = models.CharField(max_length=3)
+        students = models.ManyToManyField(Student)
